245621edf12dbc85428fe66718e3b5416cda0aa5
globuli94 <gallusjonathan@gmail.com>
2025-06-23 16:26:52 +0200

    removed redundant dependencies

--- lib/features/match/presentation/blocs/match_state.dart
+++ lib/features/match/presentation/blocs/match_state.dart
@@ -1,7 +1,6 @@
 // features/match/presentation/blocs/match/match_state.dart
 import 'package:equatable/equatable.dart';
 import 'package:localplayer/core/entities/profile_with_spotify.dart';
-import 'package:localplayer/features/match/domain/entities/user_profile.dart';
 
 
 abstract class MatchState extends Equatable {
