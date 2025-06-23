360949973d296ec415d4ec514714226bb3f34c2a
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2025-06-23 21:31:39 +0200

    spotify preview endpoint

---
+++ backend/src/spotify/SpotifyEndpoint.ts
@@ -0,0 +1,43 @@
+import { Elysia, t, status } from "elysia";
+import { AuthService } from "../authentication/AuthService.ts";
+import { mklog } from "../logger.ts";
+
+const log = mklog("spotify");
+
+log.info("Starting up Spotify Service");
+
+export const SpotifyEndpoint = new Elysia({ prefix: "/spotify" })
+    .use(AuthService)
+    .get(
+        "/preview",
+        async ({ query: { trackId } }) => {
+            log.info(`Trying to fetch preview for ${trackId}`);
+            const response = await fetch(`https://open.spotify.com/embed/track/${trackId}`);
+            const htmlPage = await response.text();
+
+            const matches = htmlPage.match(
+                /"audioPreview":\s*{\s*"url":\s*"([^"]+)"/
+            );
+            const mp3Url = matches ? matches[1] : null;
+
+            if (!mp3Url) {
+                log.error(`Could not find mp3 for sond ${trackId}`);
+                return status(404);
+            }
+
+            log.info(`Found mp3 url ${mp3Url} for song ${trackId}`);
+
+            const song = await fetch(mp3Url);
+            return await song.arrayBuffer();
+        },
+        {
+            cookie: "session",
+            requireProfile: true,
+            query: t.Object({
+                trackId: t.String(),
+            }),
+            detail: {
+                description: "Fetch a song preview mp3 from a spotify song url",
+            },
+        }
+    );
