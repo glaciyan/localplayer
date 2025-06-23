e52a4c0670f56301a1ac489d455a03760add046d
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2025-06-23 21:41:48 +0200

    error handling for preview

--- backend/src/spotify/SpotifyEndpoint.ts
+++ backend/src/spotify/SpotifyEndpoint.ts
@@ -11,25 +11,34 @@ export const SpotifyEndpoint = new Elysia({ prefix: "/spotify" })
     .get(
         "/preview",
         async ({ query: { trackId } }) => {
-            // https://stackoverflow.com/a/79238027
-            log.info(`Trying to fetch preview for ${trackId}`);
-            const response = await fetch(`https://open.spotify.com/embed/track/${trackId}`);
-            const htmlPage = await response.text();
+            try {
+                // https://stackoverflow.com/a/79238027
+                log.info(`Trying to fetch preview for ${trackId}`);
+                const response = await fetch(
+                    `https://open.spotify.com/embed/track/${trackId}`
+                );
+                const htmlPage = await response.text();
 
-            const matches = htmlPage.match(
-                /"audioPreview":\s*{\s*"url":\s*"([^"]+)"/
-            );
-            const mp3Url = matches ? matches[1] : null;
+                const matches = htmlPage.match(
+                    /"audioPreview":\s*{\s*"url":\s*"([^"]+)"/
+                );
+                const mp3Url = matches ? matches[1] : null;
 
-            if (!mp3Url) {
-                log.error(`Could not find mp3 for song ${trackId}`);
+                if (!mp3Url) {
+                    log.error(`Could not find mp3 for song ${trackId}`);
+                    return status(404);
+                }
+
+                log.info(`Found mp3 url ${mp3Url} for song ${trackId}`);
+
+                const song = await fetch(mp3Url);
+                return await song.arrayBuffer();
+            } catch (error) {
+                log.error(
+                    `Failed to fetch spotify song preview for ${trackId}`
+                );
                 return status(404);
             }
-
-            log.info(`Found mp3 url ${mp3Url} for song ${trackId}`);
-
-            const song = await fetch(mp3Url);
-            return await song.arrayBuffer();
         },
         {
             cookie: "session",
@@ -38,7 +47,8 @@ export const SpotifyEndpoint = new Elysia({ prefix: "/spotify" })
                 trackId: t.String(),
             }),
             detail: {
-                description: "Fetch a song preview mp3 from a spotify track ID.",
+                description:
+                    "Fetch a song preview mp3 from a spotify track ID.",
             },
         }
     );
