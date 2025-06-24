8a90a541aeac5c2550ab2dec25e59bbcb85ee5f0
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2025-06-24 19:47:30 +0200

    fix pubspec and preview contrainer errors

--- lib/features/profile/presentation/widgets/spotify_tracks_section.dart
+++ lib/features/profile/presentation/widgets/spotify_tracks_section.dart
@@ -21,13 +21,11 @@ class SpotifyTracksSection extends StatelessWidget {
         Text(profile.artist.name, style: Theme.of(context).textTheme.titleMedium),
         Text(profile.artist.genres, style: Theme.of(context).textTheme.bodySmall),
         const SizedBox(height: 12),
-        for (final String id in profile.artist.tracks.take(3).map((final TrackEntity track) => track.id))
+        for (final TrackEntity track in profile.artist.tracks.take(3))
           Padding(
             padding: const EdgeInsets.only(bottom: 16),
-            child: SpotifyPreviewContainer(trackId: id),
+            child: SpotifyPreviewContainer(track: track),
           ),
       ],
     );
-
-  
 }
